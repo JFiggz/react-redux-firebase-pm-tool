920186b421b1f0cb7f68c4f9b8fcd9302887395b
Figgz <jimfigliomeni@gmail.com>
2021-01-08 08:59:32 -0800

    Connect Firebase and Firestore to Redux store

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/main.css';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './store/reducers/rootReducer';
 import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, 
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig),
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
