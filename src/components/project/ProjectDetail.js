f1b54ba9022228275996d830d4907b4dafa3a2d6
Figgz <jimfigliomeni@gmail.com>
2021-01-10 21:54:44 -0800

    Connect project detail component to Firestore state values

--- src/components/project/ProjectDetail.js
+++ src/components/project/ProjectDetail.js
@@ -1,26 +1,34 @@
 import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
-function ProjectDetail({project}){
-
-    return project && (
+function ProjectDetail({project, match}){
+    return project ? (
         <section className='container proj-detail'>
             <div className='proj-detail__container'>
-                <h1 className='proj-detail__title'>Project - {project.id}</h1>
+                <h1 className='proj-detail__title'>{`${project.title} - ${match.params.id}`}</h1>
                 <p className='proj-detail__details'>{project.details}</p>
-                <p className='proj-detail__text'>Posted by Jim</p>
-                <p className='proj-detail__text'>Jan 6, 2021</p>
+                <p className='proj-detail__text'>{`Posted by ${project.authorFirstName} ${project.authorLastName}`}</p>
+                <p className='proj-detail__text'>{project.createdOn.toDate().toLocaleDateString('en-US')}</p>
             </div>
         </section>
-    );
+    )
+    :
+        <h1 className='container proj-detail__title'>Loading Project...</h1>
+    ;
 };
 
 const mapStateToProps = (state, ownProps) => {
     const id = ownProps.match.params.id;
+    const fsProjects = state.firestore.data.projects;
 
-    return({
-        project: state.project.projects.filter(proj => proj.id === id)[0],
-    });
+    return fsProjects ? ({
+        project: fsProjects[id],
+    }) : null;
 
 };
 
-export default connect(mapStateToProps)(ProjectDetail);
+export default compose(
+    firestoreConnect(() => ['projects']),
+    connect(mapStateToProps)
+)(ProjectDetail);
--- src/components/project/ProjectList.js
+++ src/components/project/ProjectList.js
@@ -5,7 +5,7 @@ export default function ProjectList({projects}){
         <section className='projects'>
             <ul className='projects__list'>
                 {projects ? projects.map(proj => {
-                    return <ProjectSummary key={proj.id} id={proj.id} title={proj.title} poster="Jim" date="January 6th 2021" />
+                    return <ProjectSummary key={proj.id} id={proj.id} title={proj.title} poster={`${proj.authorFirstName} ${proj.authorLastName}`} date={proj.createdOn.toDate().toLocaleDateString('en-US')} />
                 }) 
                 :
                     <h2 className='projects__title'>No Project Data</h2>
