cbf95482ae69be5decdc8153bbcc28c7d2a32be5
Figgz <jimfigliomeni@gmail.com>
2021-01-12 16:57:30 -0800

    Add extend styles for font sizing and style navbar

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -17,10 +17,10 @@ function SignedInLinks({signOutUser, initials}){
                 <NavLink to='/create' className='nav__link'>New Project</NavLink>
             </li>
             <li className='nav__list-item'>
-                <button type="button" className='nav__link' onClick={() => handleSignOut()} >Log Out</button>
+                <button type="button" className='nav__btn' onClick={() => handleSignOut()} >Log Out</button>
             </li>
-            <li className='nav__list-item'>
-                <NavLink to='/' className='nav__link'>{initials}</NavLink>
+            <li className='nav__list-item nav__list-item--initials'>
+                <NavLink to='/' className='nav__link nav__link--initials'>{initials}</NavLink>
             </li>
         </ul>
     );
--- src/components/project/ProjectSummary.js
+++ src/components/project/ProjectSummary.js
@@ -5,8 +5,8 @@ export default function ProjectSummary({id, title, poster, date}){
         <li className='projects__list-item'>
             <Link to={`/project/${id}`} className='projects__link'>
                 <h2 className='projects__title'>{title}</h2>
-                <p className='projects__text'>Posted by: {poster}</p>
                 <p className='projects__text projects__text--date'>{date}</p>
+                <p className='projects__text'>Posted by: {poster}</p>
             </Link>
         </li>
     );
